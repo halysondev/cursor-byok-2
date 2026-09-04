72ca2d0cf4fe61528bcef868ce9cc482cb705275
Xiashangning <1750546761@qq.com>
2026-09-04 20:44:47 +0800

    fix(plugins): package kimi token.ts in the built-in bundle
    (cherry picked from commit 8f93fd8bba4838e3cc04fc78287db0a2a2805ebc)

--- server/src/plugin/builtin.rs
+++ server/src/plugin/builtin.rs
@@ -219,6 +219,13 @@ const KIMI_AUTH: &[(&str, &str)] = &[
         )),
     ),
     (
+        "token.ts",
+        include_str!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/plugins/build-in/kimi-auth/token.ts"
+        )),
+    ),
+    (
         "resources.ts",
         include_str!(concat!(
             env!("CARGO_MANIFEST_DIR"),
